3c4c3a8c4d7aea2c45b492449b02235c5de0f7fb
SonKing002 <gur00229@gmail.com>
2023-12-19 14:10:33 +0900

    수정

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,8 +7,8 @@ using Unity.VisualScripting;
 public class NewBehaviourScript : MonoBehaviour
 {
 
-    public int price;
-    [Range(0,100)]public int count; //ex 오디오
+    public int my_Price;
+    [Range(0,99)]public int count; //ex 오디오
     // <- 갯수 제한 필요: 사용자 런타임 버그 방지
 
     public string my_Name;  
@@ -25,7 +25,15 @@ public class NewBehaviourScript : MonoBehaviour
     //시작 1프레임에 한번 , 실행 진입점
     void Start()
     {
-        price = 1_000_000; // 
+        my_Price = 1_000; // 
+        my_Name = "가그린";
+        my_Description = "시원하고 상쾌한 사용감";
+
+        //명령어코드
+        Debug.Log($"가격 : {my_Price}");
+        // Log a Message to the Unity
+        Debug.Log("이름 : " + my_Name);
+        Debug.Log(string.Format("{0}",my_Description));
     }
 
     //매 프레임마다 호출됨
@@ -63,7 +71,7 @@ B를 A에서 재 작성하지 않아도 사용 가능
 파라미터(매개변수)를 넣을 떄 사용 // 없으면 비워둠
 
     {} 중괄호(블록,body)
-코드블록 내용작성하는 단위 공간범위
+코드블록 내용작성하는 단위 공간범위, 작업범위
 
     [] 대괄호
 프로그램에서 데이터의 묶음,배열을 표현시 사용기호
@@ -73,6 +81,14 @@ B를 A에서 재 작성하지 않아도 사용 가능
 
     = 할당 대입 (같다 의미 x)
 Left Value = Right Value; Lv에 Rv 값을 넣는다. 
+
+    $ 
+문장 안에서 데이터를 확인할 수 있는 포매팅(formating)기능을
+가지고 있는 문자 
+//format : 양식 서식
+
+    C# 지원 : + 
+더하기가 문자열 뒤 연결해주는 기능으로 사용 가능
    
  */
 
@@ -90,6 +106,20 @@ Left Value = Right Value; Lv에 Rv 값을 넣는다.
 
 [Range(float,float)] public 자료형 변수명; //인스펙터 범위 설정
 [TextAria] public string 변수명; //인스펙터에서 여러줄
+
+ Debug.Log(); 메서드 : console창에 출력 메세지를 보내는 기능
+표현방식_
+1. Debug.Log($"{변수명}");
+2. Debug.Log("내용 "+변수명);
+3. Debug.Log(string.Format("{0}",변수명));
+
+//0부터 시작하는 첨자, 첨자 요소에 들어가는 변수명
+//첨자 갯수 만큼, 데이터를 넣어준다.
+
+포맷 == 양식 서식
+포매팅 : 형식을 바꿔주는 것
+
+ string.Format()메서드
  */
 
 /*Unity 스크립트 팁
@@ -135,10 +165,10 @@ ex. int float string...
 
     1_1. 할당/대입 (ex. 변수명 = 값;)
         선언 후, 변수 명만 사용한다.
-
  
     2. 선언 + 할당 == 초기화 (ex. 자료형 + 변수명 = 값;)
         이름과 값을 적는다
+
 
     변수 명의 길이 제한이 있는가 : x 
     이름은 지관적 + 간단명료하게 작성할 것
@@ -150,7 +180,8 @@ ex 정수로 해석하기 떄문
     이름에 특수문자 사용 가능하는가 : O (일부분만 가능)
 ex 특수문자의 기능이 있기 때문 일부만 가능 _(언더스코어)
 *파이썬은 쉘언어이기때문에 특수문자 불가능*
+
+    변수의 이름으로 키워드를 사용 할 수 없다.
+ex string string;
+
 */
-
-
- // 이렇게 적으면
